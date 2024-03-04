4ce195a00307e5f33ef9e01651ec9fa24738da5b
kfordhamweir@gmail.com <kfordhamweir@gmail.com>
2024-03-04 08:37:53 +0000

    Edited Dashboard

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -4,13 +4,15 @@ import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import plotly.graph_objects as go
-from ..GoogleSheetsIntegration import * 
+from .. import GoogleSheetsIntegration as gSheet
+
 
 class Dashboard(DashboardTemplate):   
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         #self.dash_transaction_repeating.items = GoogleSheets
         self.init_components(**properties)
-        print(GoogleSheetsIntegration.GetData(2['TransactionID']))
-        self.lbl_test.text = GoogleSheetsIntegration.GetData(2['TransactionID'])
+        transaction_data = gSheet.GetData(2['TransactionID'])
+        print(transaction_data)
+        self.lbl_test.text = transaction_data
         # Any code you write here will run before the form opens.
