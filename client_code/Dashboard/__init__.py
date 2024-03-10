cc9ca0f295ebc5b0985ab845a36076f5f7598ca8
kfordhamweir@gmail.com <kfordhamweir@gmail.com>
2024-03-10 09:59:46 +0000

    Edited Dashboard.RowTemplate1 and Dashboard

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -12,9 +12,13 @@ class Dashboard(DashboardTemplate):
         # Set Form properties and Data Bindings.
          #Load the googlesheet
         transaction_data = gSheet.GetData()
-        print (transaction_data.cells)
 
-        self.dash_transaction_repeating.items = transaction_data.cells
+        self.dash_transaction_repeating.items =[
+        {'name': 'Alice', 'address': '1 Road Street'},
+        {'name': 'Bob', 'address': '2 City Town'}
+        ]
+
+      #  [<Google Worksheet Row: {'TransactionID': '1', 'Date': '01/01/2024', 'Expense': 'McDonalds', 'Value': '£17.39', 'Catagory': 'Eating Out'}>, <Google Worksheet Row: {'TransactionID': '2', 'Date': '02/01/2024', 'Expense': 'Tesco', 'Value': '£62.05', 'Catagory': 'Weekly Shop'}>
 
         self.init_components(**properties)
 
